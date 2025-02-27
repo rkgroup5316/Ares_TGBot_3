d0ad9345f6961ee07777e833bd0ffa0ad6fb867f
Rkgroup <110547855+RKgroupkg@users.noreply.github.com>
2025-02-27 09:47:47 +0530

    Update keep_alive.py

--- utils/helper/keep_alive.py
+++ utils/helper/keep_alive.py
@@ -1,6 +1,5 @@
 # =============================[ UPTIME ISSUE FIXED ]================================#
 from os import environ
-import logging
 import requests
 from flask import Flask
 from threading import Thread

abc355413cd561d56a53f5f40f4ca182b28fa99d
Rkgroup <110547855+RKgroupkg@users.noreply.github.com>
2025-02-27 09:10:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -120,7 +120,7 @@ async def post_init(application: Application) -> None:
 def main() -> None:
     """Run the bot."""
     # Create the Application and pass it your bot's token.
-    application = Application.builder().token(TLG_TOKEN).concurrent_updates(True).post_init(post_init).build()
+    application = Application.builder().token(TLG_TOKEN).concurrent_updates(True).job_queue(None).post_init(post_init).build()
     
    
     message_handler = MessageHandler(filters.TEXT & ~filters.COMMAND & ~filters.Entity(MessageEntity.MENTION)& ~filters.REPLY & ~filters.Entity(MessageEntity.TEXT_MENTION), process_message)
